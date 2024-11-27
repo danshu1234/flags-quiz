63e0be1a0ec2605f6b2e8214d7e7c3534e78b299
Daniil Shundeev <danya.shundeev@internet.ru>
2024-11-27 21:23:32 +0500

    locastorage inside useEffect

--- src/app/Homepage.tsx
+++ src/app/Homepage.tsx
@@ -1,36 +1,51 @@
-'use client'
+'use client';
 
-import { FC } from "react"
+import { FC, useEffect, useState } from "react";
 import Link from "next/link";
-import './home.css'
+import './home.css';
 
 const Homepage: FC = () => {
-  let btnToSecondLevel;
-  let btnToThirdLevel;
+  const [btnToSecondLevel, setBtnToSecondLevel] = useState<JSX.Element | null>(null);
+  const [btnToThirdLevel, setBtnToThirdLevel] = useState<JSX.Element | null>(null);
 
-  const openSecond = localStorage.getItem('openSecond')
+  useEffect(() => {
+    const openSecond = localStorage.getItem('openSecond');
+    const openThird = localStorage.getItem('openThird');
 
-   if (openSecond === 'true') {
-    btnToSecondLevel = <Link href={'/second-level'}><button className="to-second-level">2 уровень</button></Link>
-   } else {
-    btnToSecondLevel = <button className="inactive-btn-2">2 уровень</button>
-   }
+    if (openSecond === 'true') {
+      setBtnToSecondLevel(
+        <Link href={'/second-level'}>
+          <button className="to-second-level">2 уровень</button>
+        </Link>
+      );
+    } else {
+      setBtnToSecondLevel(
+        <button className="inactive-btn-2">2 уровень</button>
+      );
+    }
 
-  const openThird = localStorage.getItem('openThird')
-
-   if (openThird === 'true') {
-    btnToThirdLevel = <Link href={'/third-level'}><button className="to-third-level">3 уровень</button></Link>
-   } else {
-    btnToThirdLevel = <button className="inactive-btn-3">3 уровень</button>
-   }
+    if (openThird === 'true') {
+      setBtnToThirdLevel(
+        <Link href={'/third-level'}>
+          <button className="to-third-level">3 уровень</button>
+        </Link>
+      );
+    } else {
+      setBtnToThirdLevel(
+        <button className="inactive-btn-3">3 уровень</button>
+      );
+    }
+  }, []);
 
   return (
     <div>
-      <Link href={'/first-level'}><button className="to-first-level">1 уровень</button></Link>
+      <Link href={'/first-level'}>
+        <button className="to-first-level">1 уровень</button>
+      </Link>
       {btnToSecondLevel}
       {btnToThirdLevel}
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
